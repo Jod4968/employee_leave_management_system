f7d629367c9f61629de1492d22d95c73d98450ab
Shailesh Rawat <shaileshrawat414@gmail.com>
2026-04-26 13:44:58 +0530

    Update main.c

--- main.c
+++ main.c
@@ -9,6 +9,45 @@ struct Leave {
 };
 
 
+void updateLeave() {
+    FILE *fp = fopen("leave.txt", "r");
+    FILE *temp = fopen("temp.txt", "w");
+
+    struct Leave l;
+    int id, found = 0, choice;
+
+    printf("\nEnter ID to update: ");
+    scanf("%d", &id);
+
+    while (fscanf(fp, "%d %s %d %s", &l.id, l.name, &l.days, l.status) != EOF) {
+
+        if (l.id == id) {
+            found = 1;
+
+            printf("\n1. Approve\n2. Reject\nChoose: ");
+            scanf("%d", &choice);
+
+            if (choice == 1)
+                strcpy(l.status, "Approved");
+            else
+                strcpy(l.status, "Rejected");
+        }
+
+        fprintf(temp, "%d %s %d %s\n", l.id, l.name, l.days, l.status);
+    }
+
+    fclose(fp);
+    fclose(temp);
+
+    remove("leave.txt");
+    rename("temp.txt", "leave.txt");
+
+    if (found)
+        printf(" Status Updated Successfully!\n");
+    else
+        printf(" Record Not Found!\n");
+}
+
 // View Leaves
 void viewLeaves() {
     FILE *fp = fopen("leave.txt", "r");
